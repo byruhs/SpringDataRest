fbbe0497713efb057b3fec59b9a9489fcb11ba51
Byron Delgado <bdelgado@cmnhospitals.org>
2022-07-12 16:17:43 -0600

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into alien values (101,'Juan','C#');
+insert into alien values (102,'Pedro','Android');
+insert into alien values (103,'Jose','Python');
+insert into alien values (104,'Maria','Java');
+insert into alien values (105,'Juan','PHP');
