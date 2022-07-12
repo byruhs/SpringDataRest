152ae67105e93f6e4a4a35e6b355184b418c441d
Byron Alex <ingalexby90@gmail.com>
2022-07-12 16:25:50 -0600

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
+insert into alien values (101,'Byron','Azure');
 insert into alien values (101,'Juan','C#');
 insert into alien values (102,'Pedro','Android');
 insert into alien values (103,'Jose','Python');
